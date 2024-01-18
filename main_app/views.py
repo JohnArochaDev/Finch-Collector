2b352b31e8f82b2e18c32f7e17a055211113e473
John Arocha Dev <jparocha777@gmail.com>
2024-01-18 15:17:23 -0500

    aws work

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from .models import Finch, Toy
+from .models import Finch, Toy, Photo
 from .forms import FeedingForm
 # Create your views here.
 
@@ -47,6 +50,22 @@ def add_feeding(request, finch_id):
     # finally, redirect to the cat detail page
     return redirect('detail', finch_id=finch_id)
 
+
+def add_photo(request, finch_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid3().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            bucket = os.environ['S3_BUCKET']
+            s3.upload_fileobj(photo_file, bucket, key)
+            url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+            Photo.objects.create(url=url, finch_id=finch_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3')
+            print(e)
+    return redirect('detail', finch_id=finch_id)
+
 class ToyList(ListView):
     model = Toy
     template_name = 'toys/index.html'
@@ -73,4 +92,3 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
     model = Toy
     success_url = '/toys'
-    
