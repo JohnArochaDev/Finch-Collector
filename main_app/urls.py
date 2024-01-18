a90ed9266a688450d6313f2137741d6a0d67f123
John Arocha Dev <jparocha777@gmail.com>
2024-01-18 14:10:50 -0500

    finch full functionality added

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('finch/', views.finch_index, name='index'),
-    path('finch/<int:finch_id>/', views.finch_detail, name='detail')
+    path('finch/<int:finch_id>/', views.finch_detail, name='detail'),
+    path('finch/create/', views.FinchCreate.as_view(), name='finch_create'),
+    path('finch/<int:pk>/update', views.FinchUpdate.as_view(), name='finch_update'),
+    path('finch/<int:pk>/delete', views.FinchDelete.as_view(), name='finch_delete'),
+    path('finch/<int:finch_id>/add_feeding', views.add_feeding, name='add_feeding'),
+    
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Finch
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
 # Create your views here.
 
 def home(request):
@@ -14,4 +18,59 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finch/detail.html', {'finch' : finch})
+    feedingform = FeedingForm()
+    return render(request, 'finch/detail.html', {'finch' : finch, 'feeding_form': feedingform })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['name', 'bird', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finch'
+
+def add_feeding(request, finch_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # it's also important to validate forms.
+    # django gives us a built in function for that
+    if form.is_valid():
+        # dont want to save the feeding to the db until we have a cat id
+        new_feeding = form.save(commit=False)
+        # this is where we add the cat id
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    # finally, redirect to the cat detail page
+    return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+    model = Toy
+    template_name = 'toys/index.html'
+
+# ToyDetail
+class ToyDetail(DetailView):
+    model = Toy
+    template_name = 'toys/detail.html'
+
+# ToyCreate
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+# ToyUpdate
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+    
+# ToyDelete
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys'
+    
